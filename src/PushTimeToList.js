9d17bfd00465cc9a900ce1cccd009237ec52d2ce
BZhong7 <zhong.brandon26@gmail.com>
2021-06-24 18:45:12 -0400

    Corrections to calculating working hours

--- src/PushTimeToList.js
+++ src/PushTimeToList.js
@@ -3,14 +3,16 @@ import datalog from "./HOS log.json";
 function PushTimeToList(i, k, list, daysPassed) {
     let start = new Date(datalog[i].data[k].startTime);
     let end = new Date(datalog[i].data[k].endTime);
-    let diff = end.getHours() - start.getHours();
+
+    let activeDuration = (datalog[i].data[k].dutyStatusDurations.activeDurationMs);
+    let hours = Math.round(activeDuration / 36e5);
 
     list.push(<ul>Day: {daysPassed.toString()}</ul>)
     list.push(<ul>Start Time: {start.toString()}</ul>)
     list.push(<ul>End Time: {end.toString()}</ul>)
-    list.push(<ul>Hours Worked Today: {diff.toString()}</ul>)
+    list.push(<ul>Hours Worked Today: {hours}</ul>)
 
-    return (diff)
+    return (hours)
 }
 
 export default PushTimeToList;
